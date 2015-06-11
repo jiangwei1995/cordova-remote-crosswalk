b1d578f2d7b2aa41c63e9956c81b1fe7db028186
Gen Liu <gg@linux-o4mm.site>
2015-06-11 21:59:11 +0800

    add pack

--- www/src/download.js
+++ www/src/download.js
@@ -10,52 +10,55 @@ var app = {
   progressBar: $('#progress-bar'),
   progress: 0,
 
-  initialize: function() {
+  initialize: function initialize() {
     this.readyCheck();
     this.registerPromise();
   },
 
-  registerPromise: function () {
+  registerPromise: function registerPromise() {
     app._ready.promise.then(app.downloadAPK);
   },
 
-  readyCheck: function() {
-    document.addEventListener('deviceready', function () {
+  readyCheck: function readyCheck() {
+    document.addEventListener('deviceready', function handleDeviceReady() {
       console.log('device ready');
       var downloadURL = decodeURIComponent(window.location.hash).substr(1);
       app._ready.resolve(downloadURL);
     }, false);
   },
 
-  downloadAPK: function (url) {
+  downloadAPK: function downloadAPK(url) {
     console.log('downloading:', url);
     var transfer = new FileTransfer();
     transfer.onprogress = app.onProgress;
-    transfer.download(url, TEMPORARY_URL, app.installAPK, function () {
-      console.log('err:', arguments);
-    });
+    transfer.download(url, TEMPORARY_URL, app.installAPK, handleDownload);
     setInterval(app.updateProgressBar, 200);
+    return;
+
+    function handleDownload() {
+      console.log('err:', arguments);
+    }
   },
 
-  updateProgressBar: function () {
+  updateProgressBar: function updateProgressBar() {
     app.progressBar.text(app.progress + '%');
     app.progressBar.width(app.progress + '%');
   },
 
-  onProgress: function (progressEvent) {
+  onProgress: function onProgress(progressEvent) {
     app.progress = Math.floor(progressEvent.loaded * 100 / progressEvent.total);
   },
 
-  installAPK: function (entry) {
+  installAPK: function installAPK(entry) {
     cordova.plugins.disusered.open(entry.toURL(), app.ok, app.cancel);
   },
 
-  ok: function () {
+  ok: function ok() {
     console.log('ok');
     window.location.replace('./index.html');
   },
 
-  cancel: function () {
+  cancel: function cancel() {
     console.log('cancel');
   }
 
--- www/src/index.js
+++ www/src/index.js
@@ -18,26 +18,26 @@ var app = {
   updateURL: null,
   platform: null,
 
-  initialize: function() {
+  initialize: function initialize() {
     // Promise.longStackTraces();
     this.readyCheck();
     this.registerPromise();
   },
 
-  registerPromise: function () {
+  registerPromise: function registerPromise() {
     app._ready.promise.then(app.networkCheck);
     app._ready.promise.then(app.getCurrentVersion);
     app._ready.promise.then(app.getServerVersion);
 
     app._network.promise.catch(app.noConnection);
-    app._serverVersion.promise.catch(function () {
+    app._serverVersion.promise.catch(function() {
       console.log('unable to reach version server');
     });
 
     Promise.all([
       app._currentVersion.promise,
       app._serverVersion.promise,
-    ]).spread(app.versionCheck).catch(function () {
+    ]).spread(app.versionCheck).catch(function() {
       console.log('version error:', arguments);
       app._noUpdate.resolve();
     });
@@ -48,77 +48,79 @@ var app = {
     ]).then(app.approve);
   },
 
-  readyCheck: function() {
-    document.addEventListener('deviceready', function () {
+  readyCheck: function readyCheck() {
+    document.addEventListener('deviceready', function() {
       console.log('device ready');
       app._ready.resolve();
     }, false);
   },
 
-  networkCheck: function () {
+  networkCheck: function networkCheck() {
     var targetURL = app.getAppURL();
     console.log('checking:', targetURL);
-    $.get(targetURL).then(function () {
+    $.get(targetURL).then(function() {
       console.log('server ok');
       app._network.resolve();
-    }, function (result) {
+    }, function(result) {
       console.log('server fault');
       app._network.reject('no network');
     });
   },
 
-  getCurrentVersion: function () {
-    cordova.getAppVersion.getVersionNumber().then(function (result) {
+  getCurrentVersion: function getCurrentVersion() {
+    cordova.getAppVersion.getVersionNumber().then(function(result) {
       console.log('current version', result);
       app._currentVersion.resolve(result);
     });
   },
 
-  getServerVersion: function () {
+  getServerVersion: function getServerVersion() {
     var platform = device.platform.toLowerCase();
     var versionURL = VERSION_BASE_URL + VERSION_PATH + '/' + platform;
 
     console.log('checking server version:', versionURL);
-    $.get(versionURL).then(function (result) {
+    $.get(versionURL).then(function(result) {
       console.log('server version', result.version);
       app.updateURL = result.url;
       app._serverVersion.resolve(result);
-    }, function () {
+    }, function() {
       console.log('server version error');
       app._serverVersion.reject('no update');
     });
   },
 
-  versionCheck: function (currentVersion, serverInfo) {
+  versionCheck: function versionCheck(currentVersion, serverInfo) {
     var serverVersion = serverInfo.version;
-    if (semver.lte(serverVersion, currentVersion)) throw new Error('no update');
+    if (semver.lte(serverVersion, currentVersion)) {
+      throw new Error('no update');
+    }
 
     app.buildMessage(serverInfo);
     var confirmer = $('#confirm-download');
 
-    setTimeout(function () {
+    setTimeout(function() {
       navigator.splashscreen.hide();
       confirmer.modal('show');
     }, 200);
   },
 
-  buildMessage: function (serverInfo) {
+  buildMessage: function buildMessage(serverInfo) {
     var version = serverInfo.version;
     var description = serverInfo.description;
     var list = serverInfo.features || [];
     var features = $('#features');
     $('#version').text(version);
     $('#description').text(description);
-    list.forEach(function (item) {
+    list.forEach(function(item) {
       features.append('<li>' + item + '</li>');
     });
   },
 
-  getAppURL: function() {
+  getAppURL: function getAppURL() {
     return BASE_URL + TARGET_PATH;
   },
 
-  update: function () {
+  update: function update() {
     switch (device.platform.toLowerCase()) {
     case 'android':
       console.log('update Android');
@@ -132,12 +134,12 @@ var app = {
     }
   },
 
-  noConnection: function () {
+  noConnection: function() {
     console.log('go offine');
     window.location.replace('no-connection.html');
   },
 
-  approve: function () {
+  approve: function() {
     var targetURL = app.getAppURL();
     console.log('launch app:', targetURL);
     navigator.splashscreen.hide();
